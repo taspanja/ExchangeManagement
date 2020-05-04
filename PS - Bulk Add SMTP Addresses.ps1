442dd650340742df10312b6ed5ea7bc6daa98e53
taspanja <44866069+taspanja@users.noreply.github.com>
2020-05-04 09:14:39 +0200

    Add SMTP Address
    Look up all users confirm if user has SMTP address if no add it

---
+++ PS - Bulk Add SMTP Addresses.ps1
@@ -0,0 +1,32 @@
+<#
+	Author: 	Keith Fenech
+	Date: 		29/04/2020
+	Version:	1.0
+
+	Description:
+	Output files with specific extention
+#>
+
+# Get aliases of all mailboxes
+$Mailboxes = Get-Mailbox -ResultSize Unlimited | where{$_.emailaddresses -like "*@midimalta.com"}
+ 
+# Email address to remove
+$IncludeSMTPAddress = "*@midimaltaPLC.mail.onmicrosoft.com"
+ 
+# Run through list of users attempting to remove smtp address
+$Counter = 0
+ForEach ($User in $Mailboxes) {
+    Try {
+        $VarMailbox = Get-Mailbox -Identity "$User"
+        $AllAddresses = $VarMailbox | Select -ExpandProperty EmailAddresses
+        $ProxyAddresses = $AllAddresses | Where {$_ -is "Microsoft.Exchange.Data.SmtpProxyAddress"} | Select -ExpandProperty SmtpAddress
+        # If the mailbox does not have the included address add it
+        If ($ProxyAddresses -notlike "$IncludeSMTPAddress") {
+            $Counter++
+	Set-Mailbox -Identity "$User" -EmailAddresses @{Add="$($User.Alias)@midimaltaPLC.mail.onmicrosoft.com"}
+        }
+    }
+    Catch {Write-Warning -Message "A problem occurred for $User"}
+}
+ 
+Write-Host -Object "Finished! The address '$IncludeSMTPAddress' was added to $Counter mailboxes" -ForegroundColor Green
---
+++ PS - Bulk Delete SMTP Addresses.ps1
@@ -0,0 +1,33 @@
+<#
+	Author: 	Keith Fenech
+	Date: 		29/04/2020
+	Version:	1.0
+
+	Description:
+	Output files with specific extention
+#>
+
+# Get aliases of all mailboxes
+$Mailboxes = Get-Mailbox -ResultSize Unlimited | where{$_.emailaddresses -like "*@thepointmalta.com"}
+ 
+# Email address to remove
+$ExcludedSMTPAddress = "*@thepointmalta.com"
+ 
+# Run through list of users attempting to remove smtp address
+$Counter = 0
+ForEach ($User in $Mailboxes) {
+    Try {
+        $VarMailbox = Get-Mailbox -Identity "$User"
+        $AllAddresses = $VarMailbox | Select -ExpandProperty EmailAddresses
+        $ProxyAddresses = $AllAddresses | Where {$_ -is "Microsoft.Exchange.Data.SmtpProxyAddress"} | Select -ExpandProperty SmtpAddress
+        # If the mailbox has the excluded address and it is not the primary SMTP address remove it
+        If (($ProxyAddresses -like "$ExcludedSMTPAddress") -and ($VarMailbox.PrimarySmtpAddress.ToString() -notlike "$ExcludedSMTPAddress")) {
+            $Counter++
+            $AddressesToKeep = $AllAddresses | Where {$_.SmtpAddress -notlike "$ExcludedSMTPAddress"}
+            Set-Mailbox -Identity "$User" -EmailAddresses $AddressesToKeep
+        }
+    }
+    Catch {Write-Warning -Message "A problem occurred for $User"}
+}
+ 
+Write-Host -Object "Finished! The address '$ExcludedSMTPAddress' was found and removed from $Counter mailboxes" -ForegroundColor Green
